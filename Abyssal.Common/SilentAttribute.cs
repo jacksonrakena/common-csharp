f436d688e25ca47450130c3effcc0aa866dedd4e
abyssal <jackson@rakena.co.nz>
2021-03-04 10:11:33 +1300

    Update to .NET 5, remove support for NS21

--- Abyssal.Common/SilentAttribute.cs
+++ Abyssal.Common/SilentAttribute.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Abyssal.Common
 {
     /// <summary>
-    ///     Indicates that the response from this type, or it's outcome, should be silent.
+    ///     Indicates that this type, or it's outcome, should be silent.
     /// </summary>
     public class SilentAttribute : Attribute
     {
