df27e7c3ecd44a82e139c0c5c946352f2ad161cc
abyssal <jackson@rakena.co.nz>
2021-03-10 10:44:18 +1300

    Added Cachable<T>

---
+++ Abyssal.Common/Cachable.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Abyssal.Common
+{
+    /// <summary>
+    ///     A utility class for representing a cache state for <see cref="T"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Cachable<T>
+    {
+        /// <summary>
+        ///     The current value of the <see cref="T"/> stored in this cachable.
+        /// </summary>
+        public T? Value { get; private set; }
+        
+        /// <summary>
+        ///     The last time that the value of <see cref="T"/> was set.
+        /// </summary>
+        public DateTimeOffset LastUpdated { get; private set; }
+        
+        /// <summary>
+        ///     Indicates the lifespan of the value of <see cref="T"/>.
+        /// </summary>
+        public TimeSpan Expiry { get; }
+
+        /// <summary>
+        ///     Creates a new <see cref="Cachable{T}"/> instance.
+        /// </summary>
+        /// <param name="value">The initial value of the internal store.</param>
+        /// <param name="lastUpdated">When the initial value was created. Usually <code>DateTimeOffset.Now</code>.</param>
+        /// <param name="expiry">When to expire the value.</param>
+        public Cachable(T? value, DateTimeOffset lastUpdated, TimeSpan expiry)
+        {
+            Value = value;
+            LastUpdated = lastUpdated;
+            Expiry = expiry;
+        }
+
+        /// <summary>
+        ///     Validates whether the internal value has expired, using <see cref="LastUpdated"/> and <see cref="Expiry"/>.
+        /// </summary>
+        [MemberNotNullWhen(false, "Value")]
+        public bool IsExpired()
+        {
+            return Value == null || DateTimeOffset.Now.ToUnixTimeSeconds() >
+                LastUpdated.ToUnixTimeSeconds() + Expiry.TotalSeconds;
+        }
+
+        /// <summary>
+        ///     Updates the value of this <see cref="Cachable{T}"/>.
+        /// </summary>
+        /// <param name="newValue">The new value to set.</param>
+        public void SetValue(T newValue)
+        {
+            Value = newValue;
+            LastUpdated = DateTimeOffset.Now;
+        }
+    }
+}
